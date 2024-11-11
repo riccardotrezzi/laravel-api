4906df9cdd34255a1b2bfcba1345ec63292f4ff1
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-11-11 17:16:02 +0100

    aggiornato

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
 {
     public function index() {
 
-        $projects = Project::get();
+        $projects = Project::with('category', 'tags');
 
         return response()->json([
             'success' => true,
